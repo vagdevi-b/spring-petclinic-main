fc6c9f274769fe8733319f58e7d92c45e10f4249
vagdevi-b <61586017+vagdevi-b@users.noreply.github.com>
2023-06-09 18:10:43 +0530

    Update check_stat.sh

--- check_stat.sh
+++ check_stat.sh
@@ -1,7 +1,7 @@
 url="http://localhost:8081"
 expected_status="200"
 http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-if [[ "$http_code" == "$expected_status" ]]; then
+if "$http_code" == "$expected_status" ; then
   echo "Accessible with status code $expected_status"
 else
   echo "Not accessible or has a different status code: $http_code"
